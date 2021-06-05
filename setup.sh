4edd753f62b82a1665bae7d793b8bca351ab2630
Daniel Williams <equipoise@gmail.com>
2021-06-05 09:25:13 -0400

    [feat][joe] Add config file for joe / jmacs --- nobackups

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
-ln -s ~/.dotfiles/.gitignore ~/.gitignore
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.emacs ~/.emacs
 ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/.dotfiles/.joerc ~/.joerc
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 echo 'source ~/.dotfiles/.bashrc\n' >> ~/.bashrc
