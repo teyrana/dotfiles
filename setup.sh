1466e5d419d162176acd1a170acc99e321939784
Daniel Williams <equipoise@gmail.com>
2022-06-16 14:39:29 -0400

    [fix][VSCode] simplifies config file path, updates keys, adds to setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 #! /bin/sh
 
-ln -sf ~/.dotfiles/emacs ~/.emacs
-ln -sf ~/.dotfiles/emacs.d ~/.emacs.d
-ln -sf ~/.dotfiles/jmacsrc ~/.jmacsrc
+# GNU Emacs configuration
+ln -sf ~/.dotfiles/emacs     ~/.emacs
+ln -sf ~/.dotfiles/emacs.d   ~/.emacs.d
+
+# jmacs config file
+ln -sf ~/.dotfiles/jmacsrc   ~/.jmacsrc
+
+# Git Configuration
 ln -sf ~/.dotfiles/gitignore ~/.gitignore
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+
+# TMux Configuration
 ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+
+# link VS.Code-Specific Configuration Files into their target locations
+ln -sf ~/.dotfiles/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -sf ~/.dotfiles/vscode/settings.json    ~/.config/Code/User/settings.json
 
 cat ~/.dotfiles/bashrc >> ~/.bashrc
