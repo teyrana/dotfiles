0235fd0bc2a07e13f40306a745a452cbc8eb2f06
Daniel Williams <equipoise@gmail.com>
2022-06-16 13:01:09 -0400

    [fix][setup] fixes paths in setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
-ln -sf ~/.dotfiles/.emacs ~/.emacs
-ln -sf ~/.dotfiles/.emacs.d ~/.emacs.d
-ln -sf ~/.dotfiles/.jmacsrc ~/.jmacsrc
-ln -sf ~/.dotfiles/.gitignore ~/.gitignore
-ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/emacs ~/.emacs
+ln -sf ~/.dotfiles/emacs.d ~/.emacs.d
+ln -sf ~/.dotfiles/jmacsrc ~/.jmacsrc
+ln -sf ~/.dotfiles/gitignore ~/.gitignore
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
 
-cat ~/.dotfiles/.bashrc >> ~/.bashrc
+cat ~/.dotfiles/bashrc >> ~/.bashrc
